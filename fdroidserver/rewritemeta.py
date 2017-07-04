7822341cb76d7bfb3916a87a1f7382bbded89058
Michael Pöhn <uniq@h4x.at>
2017-07-04 11:52:37 +0000

    Revert "Merge branch '290-make-rewrite-metadata-respect-key-word-sort-order' into 'master'"
    This reverts merge request !263

--- fdroidserver/rewritemeta.py
+++ fdroidserver/rewritemeta.py
@@ -77,8 +77,6 @@ def main():
         if not options.to and ext not in supported:
             logging.info("Ignoring %s file at '%s'" % (ext, path))
             continue
-        else:
-            logging.info("rewriting '%s' to %s" % (appid, options.to))
 
         to_ext = ext
         if options.to is not None:
