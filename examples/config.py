a8dda70b2f83dc42deeaf34cde9717d58b651dad
Marcus Hoffmann <bubuiic@aol.com>
2017-10-09 18:11:48 +0200

    example/config.py: comment all options

--- examples/config.py
+++ examples/config.py
@@ -50,27 +50,27 @@
 # The same policy is applied to the archive repo, if there is one.
 # repo_maxage = 0
 
-repo_url = "https://MyFirstFDroidRepo.org/fdroid/repo"
-repo_name = "My First F-Droid Repo Demo"
-repo_icon = "fdroid-icon.png"
-repo_description = """
-This is a repository of apps to be used with F-Droid. Applications in this
-repository are either official binaries built by the original application
-developers, or are binaries built from source by the admin of f-droid.org
-using the tools on https://gitlab.com/u/fdroid.
-"""
+# repo_url = "https://MyFirstFDroidRepo.org/fdroid/repo"
+# repo_name = "My First F-Droid Repo Demo"
+# repo_icon = "fdroid-icon.png"
+# repo_description = """
+# This is a repository of apps to be used with F-Droid. Applications in this
+# repository are either official binaries built by the original application
+# developers, or are binaries built from source by the admin of f-droid.org
+# using the tools on https://gitlab.com/u/fdroid.
+# """
 
 # As above, but for the archive repo.
 # archive_older sets the number of versions kept in the main repo, with all
 # older ones going to the archive. Set it to 0, and there will be no archive
 # repository, and no need to define the other archive_ values.
-archive_older = 3
-archive_url = "https://f-droid.org/archive"
-archive_name = "My First F-Droid Archive Demo"
-archive_icon = "fdroid-icon.png"
-archive_description = """
-The repository of older versions of applications from the main demo repository.
-"""
+# archive_older = 3
+# archive_url = "https://f-droid.org/archive"
+# archive_name = "My First F-Droid Archive Demo"
+# archive_icon = "fdroid-icon.png"
+# archive_description = """
+# The repository of older versions of applications from the main demo repository.
+# """
 
 # This allows a specific kind of insecure APK to be included in the
 # 'repo' section.  Since April 2017, APK signatures that use MD5 are
